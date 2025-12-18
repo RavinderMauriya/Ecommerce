bff8b677de32ac9ce7fc84074744ce0756ff07bd
RAVINDER <ravindermauriya6290@gmail.com>
2025-12-18 15:01:48 +0530

    new components added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import ShopCategory from "./Pages/ShopCategory.jsx";
 import Product from "./Pages/Product.jsx";
 import Cart from "./Pages/Cart.jsx";
 import LoginSignup from "./Pages/LoginSignup.jsx";
+import Footer from './Components/Footer/Footer.jsx';
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
         <Route path="/cart" element={<Cart/>}/>
         <Route path="/login" element={<LoginSignup/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   )
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero.jsx'
 import Popular from '../Components/Popular/Popular.jsx'
+import Offers from '../Components/Offers/Offers.jsx'
+import NewCollections from '../Components/NewCollections/NewCollections.jsx'
+import Newsletter from '../Components/NewsLetter/NewsLetter.jsx'
 
 const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
+      <Offers />
+      <NewCollections/>
+      <Newsletter/>
     </div>
   )
 }
